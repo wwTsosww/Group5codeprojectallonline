82e18e5336e10f6e331a10bbcff7db24e4774b15
metalsassd1 <114099857+metalsassd1@users.noreply.github.com>
2023-03-29 09:32:11 +0700

    reset backend

--- backend/routes/registerAPI.js
+++
@@ -1,9 +0,0 @@
-//ท็อป
-const express = require('express');
-const {register,login} = require('.././api/registerAPI');
-const routes = express.Router();
-
-routes.post('/register', register)
-routes.post('/login', login)
-
-module.exports = routes;
